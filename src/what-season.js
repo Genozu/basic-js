d54badc2338d0acf3345351cf999697e20a42db8
Nikita Pankov <genozu@yandex.by>
2023-12-19 18:18:28 +0300

    feat: add functional in what-season file

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  if (date.getMonth() <= 4 && date.getMonth() > 1) {
+    return 'spring';
+  }
+  if (date.getMonth() <= 7 && date.getMonth() > 4) {
+    return 'summer';
+  }
+  if (date.getMonth() <= 10 && date.getMonth() > 7) {
+    return 'autumn (fall)';
+  }
+  if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) {
+    return 'winter';
+  }
+  return "Invalid date!";
 }
 
 module.exports = {
